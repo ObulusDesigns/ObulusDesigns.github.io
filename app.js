3514e10b862f120b8ac514f90e6966e88682ed66
YosefDavidowitz <55250971+YosefDavidowitz@users.noreply.github.com>
2021-08-02 12:57:13 -0400

    8/2/21

--- app.js
+++ app.js
@@ -46,13 +46,13 @@ window.addEventListener('scroll', highlightMenu)
 window.addEventListener('click', highlightMenu)
 
 //Close mobile menu after clicking page
-const hideMobileMenu  = () => {
-    const menuBars = document.querySelector('is-active')
-    if(window.innerWidth <= 768 && menuBars){
-        menu.classList.toggle('is-active')
-        menuLinks.classList.remove('active')
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 768 && menuBars) {
+      menu.classList.toggle('is-active');
+      menuLinks.classList.remove('active');
     }
-}
-
-menuLinks.addEventListener('click', hideMobileMenu)
-navLogo.addEventListener('click', hideMobileMenu)
+  };
+  
+  menuLinks.addEventListener('click', hideMobileMenu);
+  navLogo.addEventListener('click', hideMobileMenu);
